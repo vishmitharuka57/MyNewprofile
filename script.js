c3ea9d02ff14895897bf79f4fceb3471840818eb
vishmitharuka57 <tharukavishmipk24@gmail.com>
2025-04-21 10:25:34 +0530

    edit content in script.js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings: ["Vishmi Tharuka ","Frontend Developer","Freelancer","Web Designer","Web Developer"],
+    strings: ["Vishmi Tharuka ","Frontend Developer","Freelancer","Web Designer","Web Developer", "UI UX Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
